98a4050c904b40572e7fbd19897bf0c23b8fbab3
nodder3239 <show3239@naver.com>
2023-11-15 18:11:56 +0900

    htmlworks

--- myprofile/js/js.js
+++ myprofile/js/js.js
@@ -1,11 +1,11 @@
-// alert("환영합니다.");
+//스크립트
 
 
 setInterval(myWatch, 1000);
 
 function myWatch(){
   const now = new Date(); //날짜와 시간 함께 생성
-  let time = now.toLocaleString();  //시간만 출력
+  let time = now.toLocaleString();  
 
   let watch = document.getElementById("show");
   watch.innerHTML = time;
@@ -26,4 +26,11 @@ window.onload = function(){
     setTimeout(showSlide, 3000);
   }
 
+  let wash = document.querySelector("#wash1");
+  wash.addEventListener("mouseover", function(){
+    wash.src="images/wash2.jpg";
+  });
+  wash.addEventListener("mouseout", function(){
+    wash.src="images/wash1.jpg";
+  });
 }
